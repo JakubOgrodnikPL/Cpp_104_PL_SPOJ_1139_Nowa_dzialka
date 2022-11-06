9d2ef4a7b2285a258ce8408d3b1df32aa69be3a6
Jakub Ogrodnik <81594386+JakubOgrodnikPL@users.noreply.github.com>
2022-11-06 11:38:51 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int a,D;
+
+int main()
+{
+    cin>>D;
+    for(int i=0; i<D; i++)
+    {
+        cin>>a;
+        cout<<a*a<<endl;
+    }
+    return 0;
+}
